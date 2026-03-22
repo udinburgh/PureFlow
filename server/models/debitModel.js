9da3e56ee2f951b8b00ebfa6a1d324b094225e62
Raul Khatami <raulkhatami217@gmail.com>
2026-03-22 14:48:10 +0700

    feat: tambah geolokasi, statistik per lokasi, estimasi konsumsi air, dan integrasi Weather API

--- server/models/debitModel.js
+++ server/models/debitModel.js
@@ -4,6 +4,10 @@ const debitSchema = new mongoose.Schema({
   volume: Number,
   waktu: Number,
   debit: Number,
+  lokasi: {
+    type: String,
+    default: "Unknown" // kalau user tidak isi lokasi
+  },
   createdAt: { type: Date, default: Date.now }
 });
 
